1eafda852233f762c7d902dc591435b385b6b9db
mukesh012 <balajimukeshbalaji@gmail.com>
2022-03-10 12:21:53 +0530

    test

--- src/test/java/org/test2/Oneclass.java
+++ src/test/java/org/test2/Oneclass.java
@@ -3,5 +3,11 @@ package org.test2;
 public class Oneclass {
 private void test() {
    System.out.println("done");
+   
+}
+private void test2() {
+	// TODO Auto-generated method stub
+System.out.println("test2");
+
 }
 }
